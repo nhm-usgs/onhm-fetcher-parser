ff98d7a6872f7af04b4e8c3d7850ba1d9f50dc42
Richard R. McDonald <rmcd@usgs.gov>
2019-05-14 13:06:30 -0600

    added some print statements for testing

--- pkg/fponhm.py
+++ pkg/fponhm.py
@@ -99,9 +99,11 @@ class FpoNHM:
         self.optpath = optpath
 
         os.chdir(self.iptpath)
+        print(os.getcwd())
         filenames = glob.glob('*.shp')
         self.gdf = pd.concat([gpd.read_file(f) for f in filenames]).pipe(gpd.GeoDataFrame)
         self.gdf.reset_index(drop=True, inplace=True)
+        print(filenames)
 
         self.num_hru = len(self.gdf.index)
         tmaxfile = None
