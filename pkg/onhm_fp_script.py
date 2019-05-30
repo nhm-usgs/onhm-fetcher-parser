e82e5be8c146602ca74ef2d9538fbac77fdb0091
Richard R. McDonald <rmcd@usgs.gov>
2019-05-30 13:43:10 -0600

    added arguments to script

--- pkg/onhm_fp_script.py
+++ pkg/onhm_fp_script.py
@@ -1,22 +1,45 @@
 from fponhm import FpoNHM
-print('starting Script')
-numdays = 2
-fp = FpoNHM(2)
-print('instantiated')
-ready = fp.initialize(r'../Data', r'../Output')
-if ready:
-    print('initalized\n')
-    print('running')
-    fp.run_weights()
-    print('finished running')
-    fp.finalize()
-    print('finalized')
-else:
-    print('Gridmet not updated continue with numdays -1')
-    fp.setNumdays(numdays-1)
-    print('initalized\n')
-    print('running')
-    fp.run_weights()
-    print('finished running')
-    fp.finalize()
-    print('finalized')
+import sys, getopt
+
+def main():
+    numdays = None
+    print(sys.argv[0:])
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hd:", ["help"])
+
+    except getopt.GetoptError as err:
+        print(err)
+        print('test.py -d <inputfile> -o <outputfile>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == "-h":
+            print("onhm_fp_script.py -d <numdays>")
+        elif opt in ("-d"):
+            numdays = int(arg)
+    print("numdays = ", numdays)
+
+
+
+    print('starting Script')
+    #numdays = 2
+    fp = FpoNHM(numdays)
+    print('instantiated')
+    ready = fp.initialize(r'../Data', r'../Output')
+    if ready:
+        print('initalized\n')
+        print('running')
+        fp.run_weights()
+        print('finished running')
+        fp.finalize()
+        print('finalized')
+    else:
+        print('Gridmet not updated continue with numdays -1')
+        fp.setNumdays(numdays-1)
+        print('initalized\n')
+        print('running')
+        fp.run_weights()
+        print('finished running')
+        fp.finalize()
+        print('finalized')
+if __name__ == "__main__":
+    main()
