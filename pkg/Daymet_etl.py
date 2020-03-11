4abc80e868bba02ec92382d608824a9429661a9a
McDonald <rmcd@usgs.gov>
2020-03-11 11:27:43 -0600

    successful test of Daymet_etl.py on winos

--- pkg/Daymet_etl.py
+++ pkg/Daymet_etl.py
@@ -1,9 +1,145 @@
 import argparse
 from pathlib import Path
 import xarray as xr
-import dask
+import pandas as pd
+import geopandas as gpd
+import datetime as dt
+import numpy as np
+from numpy.ma import masked
+import netCDF4
+import os
 
-def get_dm_files(idir, year):
+
+def finalize(odir, year, gdf, numdays, start_date, wght_id, mprcp, mtmax, mtmin):
+    print(os.getcwd())
+    os.chdir(odir)
+    print(os.getcwd())
+    ncfile = netCDF4.Dataset('dm_' + 'climate_' + str(year) + '.nc',
+                             mode='w', format='NETCDF4_CLASSIC')
+
+    # Global Attributes
+    ncfile.Conventions = 'CF-1.8'
+    ncfile.featureType = 'timeSeries'
+    ncfile.history = ''
+
+    sp_dim = len(gdf.index)
+
+    hruid_dim = ncfile.createDimension('hruid', sp_dim)  # hru_id
+    time_dim = ncfile.createDimension('time', numdays)  # unlimited axis (can be appended to).
+    for dim in ncfile.dimensions.items():
+        print(dim)
+
+    # Create Variables
+    time = ncfile.createVariable('time', 'f4', ('time',))
+    time.long_name = 'time'
+    time.standard_name = 'time'
+    time.units = 'days since ' + start_date.strftime("%Y-%m-%d %H:%M:%S")
+    time.calendar = 'standard'
+
+    hru = ncfile.createVariable('hruid', 'i', ('hruid',))
+    hru.cf_role = 'timeseries_id'
+    hru.long_name = 'local model hru id'
+
+
+    lat = ncfile.createVariable('hru_lat', np.dtype(np.float32).char, ('hruid',))
+    lat.long_name = 'Latitude of HRU centroid'
+    lat.units = 'degrees_north'
+    lat.standard_name = 'hru_latitude'
+
+    lon = ncfile.createVariable('hru_lon', np.dtype(np.float32).char, ('hruid',))
+    lon.long_name = 'Longitude of HRU centroid'
+    lon.units = 'degrees_east'
+    lon.standard_name = 'hru_longitude'
+
+    prcp = ncfile.createVariable('prcp', np.dtype(np.float32).char, ('time', 'hruid'))
+    prcp.long_name = 'Daily precipitation rate'
+    prcp.units = 'mm/day'
+    prcp.standard_name = 'lwe_precipitation_rate'
+    prcp.fill_value = netCDF4.default_fillvals['f8']
+
+    tmax = ncfile.createVariable('tmax', np.dtype(np.float32).char, ('time', 'hruid'))
+    tmax.long_name = 'Maximum daily air temperature'
+    tmax.units = 'degree_Celsius'
+    tmax.standard_name = 'maximum_daily_air_temperature'
+    tmax.fill_value = netCDF4.default_fillvals['f8']
+
+    tmin = ncfile.createVariable('tmin', np.dtype(np.float32).char, ('time', 'hruid'))
+    tmin.long_name = 'Minimum daily air temperature'
+    tmin.units = 'degree_Celsius'
+    tmin.standard_name = 'minimum_daily_air_temperature'
+    tmin.fill_value = netCDF4.default_fillvals['f8']
+
+
+    # fill variables with available data
+    def getxy(pt):
+        return pt.x, pt.y
+
+    centroidseries = gdf.geometry.centroid
+    tlon, tlat = [list(t) for t in zip(*map(getxy, centroidseries))]
+    # print(lon, lat)
+    time[:] = np.arange(0, numdays, dtype=np.float)
+    lon[:] = tlon
+    lat[:] = tlat
+    hru[:] = gdf[wght_id].values
+    # print(hruid, flush=True)
+    # tmax[0,:] = gdf['tmax'].values
+    # tmin[0,:] = gdf['tmin'].values
+    # prcp[0,:] = gdf['ppt'].values
+
+    tmax[:, :] = mtmax[:, :]
+    tmin[:, :] = mtmin[:, :]
+    prcp[:, :] = mprcp[:, :]
+
+
+    ncfile.close()
+    print("dataset is closed", flush=True)
+
+
+def get_dm_xy_bounds():
+    xmin = -2754.25
+    xmax = 3252.75
+    ymin = -2011.0
+    ymax = 1687.0
+    return xmin, xmax, ymin, ymax
+
+def np_get_wval(ndata, wghts, hru_id):
+    """
+    Returns weighted average of ndata with weights = grp
+    1) mdata = the subset of values associated with the gridmet id's that are mapped to hru_id.
+    2) Some of these values may have nans if the gridmet id is outside of conus so only return values
+    that are inside of conus
+    3) this means that hru's that are entirely outside of conus will return nans which will ultimately,
+    outside of this function get assigned zero's.
+    4) the value is assigned the weighted average
+    :param ndata: float array of data values
+    :param wghts: float array of weights
+    :param hru_id hru id number
+    :return: numpy weighted averaged - masked to deal with nans associated with
+            ndata that is outside of the conus.
+    """
+    mdata = np.ma.masked_array(ndata[wghts['grid_ids'].values.astype(int)],
+                               np.isnan(ndata[wghts['grid_ids'].values.astype(int)]))
+
+    # mdata = np.ma.masked_where(ndata[wghts['grid_ids'].values.astype(int)] <= 0.0,
+    #                            (ndata[wghts['grid_ids'].values.astype(int)]))
+    tmp = np.ma.average(mdata, weights=wghts['w'])
+    if tmp is masked:
+        # print('returning masked value', hru_id, mdata, wghts['w'])
+        return np.nan
+
+    else:
+        return tmp
+
+def get_gpd_from_shapefile(idir):
+    shapefiles = idir.glob('*.shp')
+    gdf = pd.concat([
+        gpd.read_file(shp)
+        for shp in shapefiles
+    ]).pipe(gpd.GeoDataFrame)
+    gdf.reset_index(drop=True, inplace=True)
+    return gdf
+
+def get_dm_files(idir, year, xmin, xmax, ymin, ymax):
     fprcp = None
     ftmax = None
     ftmin = None
@@ -12,18 +148,28 @@ def get_dm_files(idir, year):
     vtmax = 'tmax'
     vtmin = 'tmin'
 
-    fprcp = idir / vprcp / f'daymet_v3_{vprcp}_{year}_na.nc4'
-    ftmax = idir / vtmax / f'daymet_v3_{vtmax}_{year}_na.nc4'
-    ftmin = idir / vtmin / f'daymet_v3_{vtmin}_{year}_na.nc4'
+    fprcp = idir / f'daymet_v3_{vprcp}_{year}_na.nc4'
+    ftmax = idir / f'daymet_v3_{vtmax}_{year}_na.nc4'
+    ftmin = idir / f'daymet_v3_{vtmin}_{year}_na.nc4'
 
-    return xr.open_mfdataset([fprcp, ftmax, ftmin], combine='by_coords')
+    return xr.open_dataset(fprcp).sel(
+    y=slice(ymax*1000.0, ymin*1000.0),
+    x=slice(xmin*1000.0,xmax*1000.0)), \
+           xr.open_dataset(ftmax).sel(
+    y=slice(ymax*1000.0, ymin*1000.0),
+    x=slice(xmin*1000.0,xmax*1000.0)), \
+           xr.open_dataset(ftmin).sel(
+    y=slice(ymax*1000.0, ymin*1000.0),
+    x=slice(xmin*1000.0,xmax*1000.0))
 
 def main():
     idir = None
     odir = None
     dmyear = None
     wght_file = None
-
+    date = None
+    ndata = None
+    # default_double_value = 9.96920996838687e+36
     my_parser = argparse.ArgumentParser(prog='Daymet_etl',
                                         description='Convert daymet to prms cbh in netcdf format')
 
@@ -58,9 +204,52 @@ def main():
         wght_file = Path(args.weightsfile)
         if not wght_file.exists():
             print(f'Weights file: {wght_file} - does not exist -exiting')
+    date = dt.datetime(year=int(dmyear), month=1, day=1, hour=12)
+    start_date = date
+    xmin, xmax, ymin, ymax = get_dm_xy_bounds()
+    dprcp, dtmax, dtmin = get_dm_files(idir, dmyear, xmin, xmax, ymin, ymax)
+    gdf = get_gpd_from_shapefile(idir)
+    numdays = dprcp.sizes['time']
+    mprcp = np.zeros((numdays, len(gdf.index)))
+    mtmax = np.zeros((numdays, len(gdf.index)))
+    mtmin = np.zeros((numdays, len(gdf.index)))
+    lon = dprcp.lon.values
+    lat = dprcp.lat.values
+    gdf['prcp'] = 0
+    gdf['tmax'] = 0
+    gdf['tmin'] = 0
+    wght_dm = pd.read_csv(wght_file)
+    wght_id = wght_dm.columns[1]
+    unique_hru_ids = wght_dm.groupby(wght_id)
+    print(f'Using weight id: {wght_id}')
+    for i in range(numdays):
+        print(date)
+        d_year = np.zeros((3, (np.shape(lon)[1] - 2) * (np.shape(lon)[0] - 2)))
+        ndata = np.zeros((3, (np.shape(lon)[1] - 2) * (np.shape(lon)[0] - 2)))
+        ndata[0, :] = dprcp.prcp.sel(time=date).values[1:np.shape(lon)[0] - 1, 1:np.shape(lon)[1] - 1].flatten()
+        ndata[1, :] = dtmax.tmax.sel(time=date).values[1:np.shape(lon)[0] - 1, 1:np.shape(lon)[1] - 1].flatten()
+        ndata[2, :] = dtmin.tmin.sel(time=date).values[1:np.shape(lon)[0] - 1, 1:np.shape(lon)[1] - 1].flatten()
+        td = np.zeros((3, len(gdf.index)))
+        for index, row in gdf.iterrows():
+            try:
+                weight_id_rows = unique_hru_ids.get_group(row[wght_id])
+                d_year[0, index] = np.nan_to_num(np_get_wval(ndata[0, :], weight_id_rows, index + 1))
+                d_year[1, index] = np.nan_to_num(np_get_wval(ndata[1, :], weight_id_rows, index + 1))
+                d_year[2, index] = np.nan_to_num(np_get_wval(ndata[2, :], weight_id_rows, index + 1))
+            except KeyError:
+                d_year[0, index] = netCDF4.default_fillvals['f8']
+                d_year[1, index] = netCDF4.default_fillvals['f8']
+                d_year[2, index] = netCDF4.default_fillvals['f8']
 
-    dm_data = get_dm_files(idir, dmyear)
+        date += dt.timedelta(days=1)
+        # if i == 0:
+        #     break
 
-    print(dm_data)
+        mprcp[i,:] = td[0,:]
+        mtmax[i,:] = td[1,:]
+        mtmin[i,:] = td[2,:]
+
+    finalize(odir, dmyear, gdf, numdays, start_date, wght_id, mprcp, mtmax, mtmin)
+
 if __name__ == "__main__":
     main()
